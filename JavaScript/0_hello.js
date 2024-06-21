9b615bf68986c84a28848285a5736581cfc3143e
vX <okiror1vinald@gmail.com>
2024-06-21 06:20:26 +0300

    Intergating css into html

---
+++ JavaScript/0_hello.js
@@ -0,0 +1,12 @@
+// Hello world
+console.log("Hello");
+
+// Variables
+let first_name = "Peter";
+console.log(first_name);
+
+first_name = "Joseph";
+console.log(first_name);
+
+const tel = 123456789;
+console.log(tel);
